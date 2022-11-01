93f829fb37feace890297dccf5c1bca7de43399a
torres-sam <torres.sam@tamu.edu>
2022-11-01 17:04:26 -0500

    Updated readme

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Spin N' Stone Pizza
+      <img className="App-logo" src={logo} />
+      
+    </div>
+  );
+}
+
+export default App;
