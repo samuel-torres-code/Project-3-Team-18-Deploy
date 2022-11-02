be32e9a00279d4f54213be5f91334a18626cbe14
sjohnson440 <sjohnson440@tamu.edu>
2022-11-02 13:59:49 -0500

    login

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,5 +1,25 @@
+import bootstrap from "bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+
 const Login = () => {
-    return <h1>Login Page</h1>;
+    return (
+      <Form>
+        <Form.Group className="mb-3" controlId="loginUser">
+          <Form.Label>Email or Username</Form.Label>
+          <Form.Control type="email" placeholder="Email or Username" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="loginpass">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    );
   };
   
   export default Login;
