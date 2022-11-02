88a343007d43e53348f9fd162909338c1db68815
sjohnson440 <sjohnson440@tamu.edu>
2022-11-02 16:38:39 -0500

    basic login and resigter page design completed

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,25 +1,30 @@
 import bootstrap from "bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
-    return (
-      <Form>
-        <Form.Group className="mb-3" controlId="loginUser">
-          <Form.Label>Email or Username</Form.Label>
-          <Form.Control type="email" placeholder="Email or Username" />
-        </Form.Group>
+  return(
+    <Form>
 
-        <Form.Group className="mb-3" controlId="loginpass">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+      <Form.Group className="mt-3 mx-auto" controlId="loginUser" style={{width: '50%'}}>
+        <Form.Label>Email or Username</Form.Label>
+        <Form.Control type="email" placeholder="Email or Username" />
+      </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
-    );
-  };
+      <Form.Group className="mt-3 mx-auto" controlId="loginPass" style={{width: '50%'}}>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Link to={'/Home'}><Button className="btn btn-primary mx-3 mt-3" variant="primary" type="submit" style={{width: '20%'}}>Login</Button></Link>
+      <Link to={'/Register'}><Button className="btn btn-primary mx-3 mt-3" type="button" style={{width: '20%'}}>Register</Button></Link>
+
+    </Form>
+  );
+};
   
+
+
   export default Login;
---
+++ web/src/pages/Register.js
@@ -0,0 +1,40 @@
+import bootstrap from "bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const Register = () => {
+  return(
+      <Form>
+
+      <Form.Group className="mt-3 mx-auto" controlId="registerEmail" style={{width: '50%'}}>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+      </Form.Group>
+
+      <Form.Group className="mt-3 mx-auto" controlId="registerUser" style={{width: '50%'}}>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="username" placeholder="Username" />
+      </Form.Group>
+
+      <Form.Group className="mt-3 mx-auto" controlId="registerPass" style={{width: '50%'}}>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Form.Group className="mt-3 mx-auto" controlId="registerConfirmPass" style={{width: '50%'}}>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Link to={'/Home'}><Button className="btn btn-primary mx-3 mt-3" variant="primary" type="submit" style={{width: '20%'}}>Register</Button></Link>
+      <Link to={'/Login'}><Button className="btn btn-primary mx-3 mt-3" type="button" style={{width: '20%'}}>Back to Login</Button></Link>
+
+      </Form>
+  );
+};
+    
+
+
+  export default Register;
