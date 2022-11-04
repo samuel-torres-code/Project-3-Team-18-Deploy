a31f9d2f89ec58fad479ea1fe1acf9eecf1469b0
torres-sam <torres.sam@tamu.edu>
2022-11-04 11:02:15 -0500

    Added sample data

---
+++ web/src/api/ExampleData.js
@@ -0,0 +1,271 @@
+
+
+export const order_info = {
+    order_name: "Sam",
+  }
+
+export const pizzas = [
+    {
+      pizza_type: "build-your-own",
+      pizza_price: "$4.50",
+      ingredients: [
+        {
+          ingredient_id: "1",
+          ingredient_type: "Sauce",
+          ingredient_name: "Red Sauce",
+        },
+        {
+          ingredient_id: "2",
+          ingredient_type: "Cheese",
+          ingredient_name: "Mozzarella Cheese",
+        },
+        {
+          ingredient_id: "3",
+          ingredient_type: "Drizzle",
+          ingredient_name: "Olive Oil Drizzle",
+        },
+      ],
+    },
+    {
+      pizza_type: "one-topping",
+      pizza_price: "$3.50",
+      ingredients: [
+        {
+          ingredient_id: "4",
+          ingredient_type: "Drizzle",
+          ingredient_name: "Pesto Sauce",
+        },
+        {
+          ingredient_id: "5",
+          ingredient_type: "Cheese",
+          ingredient_name: "Ricotta Cheese",
+        },
+        {
+          ingredient_id: "6",
+          ingredient_type: "Meat",
+          ingredient_name: "Pepperoni",
+        },
+      ],
+    },
+  ]
+
+export const seasonal_items = [
+    {
+      item_name: "Seasonal Item 1",
+      item_price: "$9.45",
+    },
+    {
+      item_name: "Seasonal Item 1",
+      item_price: "$9.46",
+    }
+  ]
+
+  export const drink_counts = [
+    {
+      drink_type: "Fountain",
+      drink_count: 1,
+    },
+    {
+      drink_type: "Bottled",
+      drink_count: 0,
+    },
+  ];
+
+  
+  export const ingredients = [
+    {
+      "ingredient_id": 24,
+      "ingredient_name": "RedPeppers",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 19,
+      "ingredient_name": "BlackOlives",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 23,
+      "ingredient_name": "GreenPeppers",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 8,
+      "ingredient_name": "Mozzarella",
+      "ingredient_type": "Cheese"
+    },
+    {
+      "ingredient_id": 20,
+      "ingredient_name": "Spinach",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 18,
+      "ingredient_name": "RedOnions",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 21,
+      "ingredient_name": "KalmataOlives",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 22,
+      "ingredient_name": "Jalapenos",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 34,
+      "ingredient_name": "Oregano",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 37,
+      "ingredient_name": "Boxes",
+      "ingredient_type": "Other"
+    },
+    {
+      "ingredient_id": 6,
+      "ingredient_name": "Parmesan",
+      "ingredient_type": "Cheese"
+    },
+    {
+      "ingredient_id": 32,
+      "ingredient_name": "Sriracha",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 39,
+      "ingredient_name": "Napkins",
+      "ingredient_type": "Other"
+    },
+    {
+      "ingredient_id": 38,
+      "ingredient_name": "Cups",
+      "ingredient_type": "Other"
+    },
+    {
+      "ingredient_id": 31,
+      "ingredient_name": "BalsamicGlaze",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 16,
+      "ingredient_name": "ItalianSausage",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 27,
+      "ingredient_name": "Garlic",
+      "ingredient_type": "RoastedVeggies"
+    },
+    {
+      "ingredient_id": 1,
+      "ingredient_name": "Red",
+      "ingredient_type": "Sauce"
+    },
+    {
+      "ingredient_id": 7,
+      "ingredient_name": "Ricotta",
+      "ingredient_type": "Cheese"
+    },
+    {
+      "ingredient_id": 12,
+      "ingredient_name": "Salami",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 33,
+      "ingredient_name": "OliveOil",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 25,
+      "ingredient_name": "RedPotatoes",
+      "ingredient_type": "RoastedVeggies"
+    },
+    {
+      "ingredient_id": 10,
+      "ingredient_name": "ApplewoodBacon",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 5,
+      "ingredient_name": "Vegan",
+      "ingredient_type": "Cheese"
+    },
+    {
+      "ingredient_id": 30,
+      "ingredient_name": "BBQSauce",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 4,
+      "ingredient_name": "Pesto",
+      "ingredient_type": "Sauce"
+    },
+    {
+      "ingredient_id": 11,
+      "ingredient_name": "Pepperoni",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 2,
+      "ingredient_name": "White",
+      "ingredient_type": "Sauce"
+    },
+    {
+      "ingredient_id": 9,
+      "ingredient_name": "HouseBlend",
+      "ingredient_type": "Cheese"
+    },
+    {
+      "ingredient_id": 14,
+      "ingredient_name": "BlackForestHam",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 28,
+      "ingredient_name": "MixedMushrooms",
+      "ingredient_type": "RoastedVeggies"
+    },
+    {
+      "ingredient_id": 3,
+      "ingredient_name": "ZestyRed",
+      "ingredient_type": "Sauce"
+    },
+    {
+      "ingredient_id": 13,
+      "ingredient_name": "Meatball",
+      "ingredient_type": "Meats"
+    },
+    {
+      "ingredient_id": 35,
+      "ingredient_name": "BasilPesto",
+      "ingredient_type": "Drizzle"
+    },
+    {
+      "ingredient_id": 29,
+      "ingredient_name": "CaramelizedOnions",
+      "ingredient_type": "RoastedVeggies"
+    },
+    {
+      "ingredient_id": 36,
+      "ingredient_name": "Dough",
+      "ingredient_type": "Dough"
+    },
+    {
+      "ingredient_id": 17,
+      "ingredient_name": "CherryTomato",
+      "ingredient_type": "RawVeggies"
+    },
+    {
+      "ingredient_id": 26,
+      "ingredient_name": "Broccolini",
+      "ingredient_type": "RoastedVeggies"
+    },
+    {
+      "ingredient_id": 15,
+      "ingredient_name": "SmokedChicken",
+      "ingredient_type": "Meats"
+    }
+   ]
