2f40993005ea37526d3977a473124d01559a9ad5
torres-sam <torres.sam@tamu.edu>
2022-11-07 14:06:44 -0600

    Fixed Delete and Edit bug that would delete many items. Used id instead of index.

--- web/src/components/OrderCard.js
+++ web/src/components/OrderCard.js
@@ -1,5 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
+import  InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const DrinkCountRows = ({ drink_counts }) => {
   return drink_counts.map((drink, index) => (
@@ -48,18 +51,38 @@ const PizzaRows = ({ pizzas, handleDeletePizza, handleEditPizza }) => {
         ))}
       </div>
       <div className="col-2">
-        <button onClick={() => handleEditPizza(index)} className="btn btn-primary">
+        <button onClick={() => handleEditPizza(pizza.pizza_id)} className="btn btn-primary">
           <FontAwesomeIcon icon={faPencil} />
         </button>
       </div>
       <div className="col-2">
-        <button onClick={() => handleDeletePizza(index)} className="btn btn-primary">
+        <button onClick={() => handleDeletePizza(pizza.pizza_id)} className="btn btn-primary">
           <FontAwesomeIcon icon={faTrash} />
         </button>
       </div>
     </div>
   ));
 };
+
+const OrderInfo = ({order_info, createEmptyOrder}) => {
+  if (order_info.name === "") {
+    return (<InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Customer's name"
+                aria-label="Customer's name"
+                aria-describedby="customer-enter-name"
+              />
+              <Button onClick={() => createEmptyOrder} variant="outline-primary" id="customer-enter-name">
+                Start Order
+              </Button>
+          </InputGroup>
+    )
+  }
+  else {
+    return (<strong>Order Name: {order_info.name}</strong>)
+  }
+
+}
 
 const OrderCard = ({
   order_info,
@@ -69,7 +92,8 @@ const OrderCard = ({
   total_price,
   handleEditPizza,
   handleDeletePizza,
-  handleDeleteSeasonalItem
+  handleDeleteSeasonalItem,
+  createEmptyOrder
 }) => {
   
 
@@ -78,7 +102,7 @@ const OrderCard = ({
       <div className="card">
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
-            <strong>Order Name: {order_info.order_name}</strong>
+            <OrderInfo createEmptyOrder={createEmptyOrder} order_info = {order_info}/>
           </li>
           <div style={{maxHeight: '80vh', overflowY:'scroll'}} >
           <li className="list-group-item">
