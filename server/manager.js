d11545e5668c7ac6101dc5cac917d19035d9f66e
Ananth Kumar <ananthkumar2002@tamu.edu>
2022-11-08 12:08:44 -0600

    skeleton for manager api

--- server/manager.js
+++ server/manager.js
@@ -6,4 +6,38 @@ router.get('/', function(req, res){
  });
 
 
+ router.get('/load_ingredients', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/load_prices', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/add_inventory', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/add_ingredient', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/remove_ingredient', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/load_menu_items', function(req, res){
+    //TODO
+ });
+
+ 
+ router.get('/update_menu_items', function(req, res){
+    //TODO
+ });
+
  module.exports = router;
