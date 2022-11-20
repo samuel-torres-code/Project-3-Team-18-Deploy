125168296dd12a4df62bc400493aedef7e22b095
torres-sam <torres.sam@tamu.edu>
2022-11-20 12:40:14 -0600

    Added hook to create and update order info based on local storage. I added a few helper functions to abstract the complex JSON object. Example of how to use in a component can be found in Pizza.js

--- web/src/pages/Pizza.js
+++ web/src/pages/Pizza.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import ItemButton from "../components/ItemButton";
+import useOrder from "../hooks/useOrder";
+import { getIngredientsByType, getItemTypes, postOrder } from "../api/ServerAPI";
 
 const Pizza = () => {
   const buttonClick = () => {
     alert("button click");
   };
+  const [pizzaIndex,setPizzaIndex] = useState(0);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  
+  const [pizza,setPizza] = useState(null)
+  const { loading, error, order, addNewPizza,
+    updatePizza,deletePizza,addDrink} = useOrder([])
+
+    //For Testing, remove later with dynamic url
+    useEffect(() => {
+        addNewPizza()
+        setPizzaIndex(order.pizzas.length-1)
+        setPizza(order.pizzas[order.pizzas.length-1])
+        addDrink("Fountain","2.45")
+    },[])
 
   return (
     <div className="container">
