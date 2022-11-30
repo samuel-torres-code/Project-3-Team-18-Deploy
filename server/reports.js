8337bdb2257a6e255df3a59a9c5075bfbfe2a4f0
Ananth Kumar <ananthkumar2002@tamu.edu>
2022-11-30 15:23:31 -0600

    setup all routes

--- server/reports.js
+++ server/reports.js
@@ -21,3 +21,32 @@ const pool = new Pool({
 router.get('/', function(req, res){
     res.send('default route /api/reports');
 });
+
+router.get('/sales', function(req, res){
+    res.send('route /api/reports/sales');
+});
+
+router.get('/excess', function(req, res){
+    res.send('route /api/reports/excess');
+});
+
+router.get('/restock', function(req, res){
+    res.send('route /api/reports/restock');
+});
+
+router.get('/restock_all', function(req, res){
+    res.send('route /api/reports/resock_all');
+});
+
+router.post('/add_seasonal_item', function(req, res){
+    res.send('route /api/reports/add_seasonal_item');
+});
+
+router.post('/remove_seasonal_item', function(req, res){
+    res.send('route /api/reports/remove_seasonal_item');
+});
+
+
+router.get('/honors', function(req, res){
+    res.send('route /api/reports/honors');
+});
