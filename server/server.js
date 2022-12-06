9b221d1e898931caabb91cd3aea5ecc40959eb69
Ananth Kumar <ananthkumar2002@tamu.edu>
2022-12-06 17:06:00 -0600

    fix /api/server/ingredients to dynamically work with different types

--- server/server.js
+++ server/server.js
@@ -37,15 +37,7 @@ router.get('/ingredients', function(req, res) {
                 f_response.push(query_res.rows[i]);
             }
             q_resp = f_response;
-            var final_dict = {"Other": [],
-                      "Meats": [],
-                      "Cheese": [],
-                      "RoastedVeggies" : [],
-                      "Sauce": [],
-                      "Drizzle": [],  
-                      "Dough": [],
-                      "RawVeggies": []
-                    };
+            var final_dict = {};
             for(let i = 0; i < q_resp.length; i++)
             {
                 //create ingredient object for ingredient
@@ -57,7 +49,10 @@ router.get('/ingredients', function(req, res) {
                                 "ingredient_name" : ing_name
                                 };
                 //add to final dict
-                final_dict[ing_type] = final_dict[ing_type].concat([ing_obj]);
+                if(final_dict[ing_type] == null) {
+                    final_dict[ing_type] = [];
+                }
+                final_dict[ing_type].push([ing_obj]);
             } 
             //send response once populated
             res.send(final_dict);
